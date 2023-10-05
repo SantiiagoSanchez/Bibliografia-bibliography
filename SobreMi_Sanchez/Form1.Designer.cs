a4cc77f67cb0b58282e322505a2e811751c6e8a7
Sanch <Sanch@DESKTOP-NQV1S7L>
2023-10-05 20:14:07 -0300

    Agregar archivos de proyecto.

---
+++ SobreMi_Sanchez/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace SobreMi_Sanchez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblArgentina = new System.Windows.Forms.Label();
+            this.lblFutbol = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btEspañol = new System.Windows.Forms.Button();
+            this.btIngles = new System.Windows.Forms.Button();
+            this.lblidioma = new System.Windows.Forms.Label();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.lblSalir = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(369, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(130, 39);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "SOBRE MI";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblNombre.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 72);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(400, 39);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Hola! Mi nombre es Santiago Sanchez";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblEdad.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(12, 130);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(400, 39);
+            this.lblEdad.TabIndex = 3;
+            this.lblEdad.Text = "Actualmente (2023) tengo 18 años!";
+            // 
+            // lblArgentina
+            // 
+            this.lblArgentina.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblArgentina.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArgentina.Location = new System.Drawing.Point(12, 188);
+            this.lblArgentina.Name = "lblArgentina";
+            this.lblArgentina.Size = new System.Drawing.Size(400, 39);
+            this.lblArgentina.TabIndex = 4;
+            this.lblArgentina.Text = "Vivo en Argentina, Cordoba Capital.";
+            // 
+            // lblFutbol
+            // 
+            this.lblFutbol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblFutbol.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFutbol.Location = new System.Drawing.Point(12, 253);
+            this.lblFutbol.Name = "lblFutbol";
+            this.lblFutbol.Size = new System.Drawing.Size(487, 39);
+            this.lblFutbol.TabIndex = 7;
+            this.lblFutbol.Text = "Me gusta el Futbol, mi jugador favorito es Messi!";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::SobreMi_Sanchez.Properties.Resources.pelota;
+            this.pictureBox5.Location = new System.Drawing.Point(261, 295);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(238, 169);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SobreMi_Sanchez.Properties.Resources.messi;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 295);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(243, 169);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SobreMi_Sanchez.Properties.Resources.cordoba;
+            this.pictureBox3.Location = new System.Drawing.Point(572, 166);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(99, 61);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SobreMi_Sanchez.Properties.Resources.argentina;
+            this.pictureBox2.Location = new System.Drawing.Point(435, 166);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 61);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SobreMi_Sanchez.Properties.Resources.naranja;
+            this.pictureBox1.Location = new System.Drawing.Point(3, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 478);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btEspañol
+            // 
+            this.btEspañol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btEspañol.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEspañol.Location = new System.Drawing.Point(687, 51);
+            this.btEspañol.Name = "btEspañol";
+            this.btEspañol.Size = new System.Drawing.Size(107, 46);
+            this.btEspañol.TabIndex = 10;
+            this.btEspañol.Text = "ESPAÑOL";
+            this.btEspañol.UseVisualStyleBackColor = false;
+            this.btEspañol.Click += new System.EventHandler(this.btEspañol_Click);
+            // 
+            // btIngles
+            // 
+            this.btIngles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btIngles.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIngles.Location = new System.Drawing.Point(687, 103);
+            this.btIngles.Name = "btIngles";
+            this.btIngles.Size = new System.Drawing.Size(107, 46);
+            this.btIngles.TabIndex = 11;
+            this.btIngles.Text = "INGLES";
+            this.btIngles.UseVisualStyleBackColor = false;
+            this.btIngles.Click += new System.EventHandler(this.btIngles_Click);
+            // 
+            // lblidioma
+            // 
+            this.lblidioma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblidioma.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblidioma.Location = new System.Drawing.Point(622, 9);
+            this.lblidioma.Name = "lblidioma";
+            this.lblidioma.Size = new System.Drawing.Size(218, 39);
+            this.lblidioma.TabIndex = 12;
+            this.lblidioma.Text = "Cambia el idioma";
+            // 
+            // btSalir
+            // 
+            this.btSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btSalir.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalir.Location = new System.Drawing.Point(653, 332);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(114, 79);
+            this.btSalir.TabIndex = 13;
+            this.btSalir.Text = "SALIR";
+            this.btSalir.UseVisualStyleBackColor = false;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // lblSalir
+            // 
+            this.lblSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblSalir.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalir.Location = new System.Drawing.Point(599, 279);
+            this.lblSalir.Name = "lblSalir";
+            this.lblSalir.Size = new System.Drawing.Size(241, 39);
+            this.lblSalir.TabIndex = 14;
+            this.lblSalir.Text = " CERRAR PROGRAMA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 476);
+            this.Controls.Add(this.lblSalir);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.lblidioma);
+            this.Controls.Add(this.btIngles);
+            this.Controls.Add(this.btEspañol);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lblFutbol);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblArgentina);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblArgentina;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblFutbol;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button btEspañol;
+        private System.Windows.Forms.Button btIngles;
+        private System.Windows.Forms.Label lblidioma;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Label lblSalir;
+    }
+}
+
---
+++ SobreMi_Sanchez/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SobreMi_Sanchez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btEspañol_Click(object sender, EventArgs e)
+        {
+            lblTitulo.Text = "SOBRE MI";
+            lblNombre.Text = "Hola! Mi nombre es Santiago Sanchez\r\n";
+            lblEdad.Text = "Actualmente (2023) tengo 18 años!";
+            lblArgentina.Text = "Vivo en Argentina, Cordoba Capital.";
+            lblFutbol.Text = "Me gusta el Futbol, mi jugador favorito es Messi!";
+            lblidioma.Text = "Cambia el idioma";
+            lblSalir.Text = " CERRAR PROGRAMA";
+            btEspañol.Text = "ESPAÑOL";
+            btIngles.Text = "INGLES";
+            btSalir.Text = "SALIR";
+        }
+
+        private void btIngles_Click(object sender, EventArgs e)
+        {
+            lblTitulo.Text = "ABOUT ME";
+            lblNombre.Text = "Hi! My name is Santiago Sanchez\r\n";
+            lblEdad.Text = "At the moment (2023) Im 18 old";
+            lblArgentina.Text = "I live in Argentina, Cordoba Capital";
+            lblFutbol.Text = "I like Football, my favorite player is Messi!";
+            lblidioma.Text = "Change the language";
+            lblSalir.Text = " CLOSE PROGRAM";
+            btEspañol.Text = "SPANISH";
+            btIngles.Text = "ENGLISH";
+            btSalir.Text = "EXIT";
+                
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
